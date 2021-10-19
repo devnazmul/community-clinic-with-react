24b5cbec26d14cd5f71b1b4f9cd80bf0d22cfb0b
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2021-10-19 20:07:58 +0600

    fixed .env issue

--- src/firebaase/firebase.config.js
+++ src/firebaase/firebase.config.js
@@ -1,21 +1,12 @@
-// const firebaseConfig = {
-//     apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-//     authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-//     projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-//     appId: process.env.REACT_APP_FIREBASE_APPID,
-//     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
-//   };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyANNMEcHEltKicHC4dusOI7IAujF4tb-uM",
-  authDomain: "community-clinic1332.firebaseapp.com",
-  projectId: "community-clinic1332",
-  storageBucket: "community-clinic1332.appspot.com",
-  messagingSenderId: "103514297068",
-  appId: "1:103514297068:web:b5a241a0645f12c6bb8bc9",
-  measurementId: "G-NT999TCFXG"
-};
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID,
+  };
+
 
 export default firebaseConfig;
